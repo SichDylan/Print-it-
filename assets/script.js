b88f90fcb55ca346d0439c9c52c2622d6b1bc423
SichDylan <sichdylann@gmail.com>
2023-12-18 01:57:39 +0100

    Ajout des slides

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,41 @@
 const slides = [
 	{
-		"image": "slide1.jpg",
-		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+		"image":"./assets/images/slideshow/slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image": "slide2.jpg",
-		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image":"./assets/images/slideshow/slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image": "slide3.jpg",
-		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image":"./assets/images/slideshow/slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image": "slide4.png",
-		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+		"image":"./assets/images/slideshow/slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+function imgBuilder(currentSlide){
+	let newImg = document.createElement("img");
+	let banner = document.getElementById("banner");
+
+	banner.appendChild(newImg);
+
+	newImg.classList.add("banner-img");
+	newImg.src = currentSlide.image;
+	newImg.setAttribute("alt",currentSlide.tagLine);
+	
+}
+
+for (let i = 1; i < slides.length; i++) {
+	const currentSlide = slides[i];
+	imgBuilder(currentSlide);
+}
+
+
 const arrow_right = document.querySelector("#banner .arrow_right");
 const arrow_left = document.querySelector("#banner .arrow_left");
 
@@ -26,7 +46,7 @@ function arrowDirection(arrow) {
 		alert('droite');
 	}
 	else {
-		alert('gauche')
+		alert('gauche');
 	}
 }
 
@@ -35,6 +55,5 @@ navsArrows.forEach(function (arrow) {
 		arrowDirection(arrow);
 	}
 	);
-});
-
+})
 
